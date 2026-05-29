e1eb4ae22420c3bd7eff8da169444834af0dac3b
Ksuserkqy <2765301200@qq.com>
2026-05-29 21:27:42 +0800

    Initial Commit

---
+++ KsuserBookKeepingIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KsuserBookKeepingIOS
+//
+//  Created by Ksuserkqy on 2026/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KsuserBookKeepingIOS/KsuserBookKeepingIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KsuserBookKeepingIOSApp.swift
+//  KsuserBookKeepingIOS
+//
+//  Created by Ksuserkqy on 2026/5/29.
+//
+
+import SwiftUI
+
+@main
+struct KsuserBookKeepingIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
